1c4c5baa4ca25ecdba11f06f5ba743b36e8a10ed
adrian <rivendell@protonmail.com>
2020-12-28 15:34:10 +0100

    add routing, menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,50 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom'
 import './App.css';
-import { Select } from 'antd';
-import { Button } from 'antd';
-import { Input } from 'antd';
-
-import { Typography } from 'antd';
+import { Select, Button, Input, Typography } from 'antd';
 import {  CheckCircleTwoTone } from '@ant-design/icons';
 
 
 const { Text } = Typography;
-
-
 const { Option } = Select;
 
 const currencies = ["EUR","PLN","GBP", "SEK"];
 let firstCurrency = "";
 let secondCurrency = "";
+
+
+
+
+
+const Home = () => {
+  const [amount, setAmount] = useState("");
+  const handleAmountChange = (e) => {
+    
+    setAmount(e.target.value)
+  }
+    const activeCurrencies = currencies.map(currency => <Option key = {currency}value = {currency}>{currency}</Option>)
+    return (
+      <>
+        <Input placeholder="Podaj kwotę" style={{ width: '25%' }} onChange={handleAmountChange} value = {amount} />
+        <Select defaultValue="EUR" style={{ width: 120 }} onChange={handleChangeFirstCurrency}>
+        {activeCurrencies}
+        </Select>
+        <Text strong > Przelicz na: </Text>
+        <Select defaultValue="PLN" style={{ width: 120 }} onChange={handleChangeSecondCurrency}>
+        {activeCurrencies}
+        </Select>
+
+        <Button onClick = {handleSubmit}>Przelicz <CheckCircleTwoTone twoToneColor="#52c41a" /></Button>
+      </>
+  )
+}
+
+
+
+
+const History = () => <h1>History</h1>
+const Settings = () => <h1>Settings</h1>
+
 
 
 
@@ -38,37 +67,28 @@ const handleSubmit = () => {
 
 
 function App() {
-  const [amount, setAmount] = useState("");
-  const handleAmountChange = (e) => {
-
-
-    
-    setAmount(e.target.value)
-  }
-  const activeCurrencies = currencies.map(currency => <Option key = {currency}value = {currency}>{currency}</Option>)
-
-
-
   return (
-    <div className = "main">
-      <Input placeholder="Podaj kwotę" style={{ width: '25%' }} onChange={handleAmountChange} value = {amount} />
+    <Router>
+        <div className = "main">
+
+          <header>
+            <nav>
+              <ul>
+                <li><NavLink exact to="/">Main page</NavLink></li>
+                <li><NavLink to="/history">History</NavLink></li>
+                <li><NavLink to="/settings">Settings</NavLink></li>
+              </ul>
+            </nav>
+          </header>
+          <section>
+            <Route exact path = "/" component = {Home}/>
+            <Route path = "/History" component = {History}/>
+            <Route path = "/Settings" component = {Settings}/>
+          </section>
 
 
-      <Select defaultValue="EUR" style={{ width: 120 }} onChange={handleChangeFirstCurrency}>
-      {activeCurrencies}
-      </Select>
-      <Text strong > Przelicz na: </Text>
-      <Select defaultValue="PLN" style={{ width: 120 }} onChange={handleChangeSecondCurrency}>
-      {activeCurrencies}
-      </Select>
-
-      <Button onClick = {handleSubmit}>Przelicz <CheckCircleTwoTone twoToneColor="#52c41a" /></Button>
-
-
-
-      <p></p>
-
-    </div>
+        </div>
+    </Router>
   );
 }
 
